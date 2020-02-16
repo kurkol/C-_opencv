0c08523284da289816b5675c42518b6d4eab0f81
kurkol <52071654+kurkol@users.noreply.github.com>
2020-02-16 21:19:12 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+
+int main()
+
+{
+
+	//读取本地的一张图片便显示出来
+	//imread后面的文件路径可以根据自己的实际路径修改。
+
+	Mat img = imread("C:/Users/Administrator/Desktop/111.jpg");
+
+	imshow("test", img);
+
+	//等等待用户按键
+
+	waitKey(0);
+
+	return 0;
+}
