9700dcc1a5071caa195e4fbb4b2a6873402a29f3
kurkol <52071654+kurkol@users.noreply.github.com>
2020-03-03 19:19:41 +0800

    Create 1.cpp

---
+++ Exercise1/1.cpp
@@ -0,0 +1,27 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+
+int main()
+
+{
+	Mat img = imread("C:/Users/Administrator/Desktop/111.jpg");
+
+	int height = img.rows;
+	int width = img.cols;
+	for (int j = 0; j < height; j++) {
+		for (int i = 0; i < width; i++) {
+			uchar average = (img.at<Vec3b>(j, i)[0] + img.at<Vec3b>(j, i)[1] + img.at<Vec3b>(j, i)[2]) / 3;
+			img.at<Vec3b>(j, i)[0] = average;
+			img.at<Vec3b>(j, i)[1] = average;
+			img.at<Vec3b>(j, i)[2] = average;
+		}
+	}
+
+	imshow("test", img);
+
+	waitKey(0);
+
+	return 0;
+}
